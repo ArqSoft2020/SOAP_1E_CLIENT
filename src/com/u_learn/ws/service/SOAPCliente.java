639370ebc66243279c3780fc9d3050cfa3eeb3e4
Pineda <cfpinedao@unal.edu.co>
2020-06-04 20:31:20 -0500

    Arreglado main de Cliente

--- src/com/u_learn/ws/service/SOAPCliente.java
+++ src/com/u_learn/ws/service/SOAPCliente.java
@@ -11,102 +11,25 @@ import javax.xml.rpc.ServiceException;
 import javax.xml.rpc.encoding.TypeMappingRegistry;
 import javax.xml.rpc.handler.HandlerRegistry;
 
+import org.tempuri.CoursePojo;
+
 public class SOAPCliente {
 	
 	public static void main(String[] args) {
 		
-		SoapCoursesListService locator = new SoapCoursesListService() {
+		SoapCoursesListServiceLocator locator = new SoapCoursesListServiceLocator();
+		
+		try {
+			CoursesList servicioCursos = locator.getCoursesList();
+			String id="3";
+			CoursePojo[] cPojo= servicioCursos.getCourseInformation(id);
+			System.out.println("Respuesta: \nDirecc. Usuario:"+ cPojo[0].getNombre());
 			
-			@Override
-			public URL getWSDLDocumentLocation() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public TypeMappingRegistry getTypeMappingRegistry() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public QName getServiceName() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Iterator getPorts() throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Remote getPort(QName arg0, Class arg1) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Remote getPort(Class arg0) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public HandlerRegistry getHandlerRegistry() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Call[] getCalls(QName arg0) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Call createCall(QName arg0, String arg1) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Call createCall(QName arg0, QName arg1) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Call createCall(QName arg0) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public Call createCall() throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public String getCoursesListAddress() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public CoursesList getCoursesList(URL portAddress) throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public CoursesList getCoursesList() throws ServiceException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-		};
+		}catch(ServiceException e) {
+			e.printStackTrace();
+		}catch(RemoteException e) {
+			e.printStackTrace();
+		}
 	}
 	
 
